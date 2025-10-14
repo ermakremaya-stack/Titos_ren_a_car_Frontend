41d04e3b94d520859b627820ca67df638966f3fb
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2025-10-14 08:50:58 -0600

    Propotipo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "./App.css";
+import Titulo from './components/Titulo.jsx';
+
+const App = () => {
+return (
+    <>
+    <Titulo />
+    </>
+);
+};
+
+
+export default App;
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Titulo = () => {
+
+    return (
+        <>
+        <h1>Hola, Mundo</h1>
+        <h2>Bienvenidos a Tito´s rent a car</h2>
+        </>
+    );
+};
+
+export default Titulo;
