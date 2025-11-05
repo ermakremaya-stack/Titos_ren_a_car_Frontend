0d1ba12f620c191f3893a4a3b4985b48843b6e9f
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2025-11-05 15:55:44 -0600

    Prototipo de capacitación.

---
+++ src/components/capacitacion/presentaciones/ComoRegistrar.jsx
@@ -0,0 +1,26 @@
+import Carousel from "react-bootstrap/Carousel";
+import paso1 from "../../../assets/capacitacion/comoRegistrar/paso1.png";
+
+function ComoRegistrar() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img src={paso1} style={{ width: "100%", maxHeight: "450px", objectFit: "contain" }} />
+        <Carousel.Caption>
+          <h3>Paso 1</h3>
+          <p>Aquí explicas el primer paso con detalle.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img src={paso1} style={{ width: "100%", maxHeight: "450px", objectFit: "contain" }} />
+        <Carousel.Caption>
+          <h3>Paso 1</h3>
+          <p>Aquí explicas el primer paso con detalle.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ComoRegistrar;
--- src/views/Capacitacion.jsx
+++ src/views/Capacitacion.jsx
@@ -1,35 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import ComoRegistrar from "../components/capacitacion/presentaciones/ComoRegistrar.jsx"
+import BarraOpciones from "../components/capacitacion/BarraOpciones.jsx"
 
 const Capacitacion = () => {
+  const [preguntaSeleccionada, setPreguntaSeleccionada] = useState(null);
 
-  <>
-    <title>
-      ¿Que quieres saber?
-    </title>
-  </>
+  return (
+    <>
+      <BarraOpciones onSelectPregunta={setPreguntaSeleccionada} />
 
-
-
+      <div style={{ padding: "20px" }}>
+        {preguntaSeleccionada === "p1" && <ComoRegistrar />}
+      </div>
+    </>
+  );
 };
 
 export default Capacitacion;
 
 // Card https://react-bootstrap.netlify.app/docs/components/cards
 
-//Carousels https://react-bootstrap.netlify.app/docs/components/carousel sirve para mostrar imagenes en secuencia perfecto para el inicio de secion o registrpo dedatos 
+//Carousels https://react-bootstrap.netlify.app/docs/components/carousel sirve para mostrar imagenes en secuencia perfecto para el inicio de secion o registrpo dedatos
 
-//Close button https://react-bootstrap.netlify.app/docs/components/close-button 
+//Close button https://react-bootstrap.netlify.app/docs/components/close-button
 
 //Images https://react-bootstrap.netlify.app/docs/components/images para imagenes
 
-// Modals https://react-bootstrap.netlify.app/docs/components/modal  sirve para dar ventanas emergentes 
+// Modals https://react-bootstrap.netlify.app/docs/components/modal  sirve para dar ventanas emergentes
 
 // Navbars https://react-bootstrap.netlify.app/docs/components/navbar barra que desglosa mas cosas
 
 //Offcanvas https://react-bootstrap.netlify.app/docs/components/offcanvas  Desglosa la barra lateral izquierda
 
-//PLaceholders https://react-bootstrap.netlify.app/docs/components/placeholder 
+//PLaceholders https://react-bootstrap.netlify.app/docs/components/placeholder
 
-// Tabbed components  https://react-bootstrap.netlify.app/docs/components/tabs 
+// Tabbed components  https://react-bootstrap.netlify.app/docs/components/tabs
 
 //Toast https://react-bootstrap.netlify.app/docs/components/toasts Sirve para mandar mensajes
