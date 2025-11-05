3015636dedf460eb19be3998832b5058f6cc5c6a
Ernesto-Sevilla <estudianteernestosevilla@gmail.com>
2025-11-05 12:13:34 -0600

    Planificación de capacitacion

--- src/views/Capacitacion.jsx
+++ src/views/Capacitacion.jsx
@@ -1,6 +1,35 @@
 import React from "react";
 
 const Capacitacion = () => {
+
+  <>
+    <title>
+      ¿Que quieres saber?
+    </title>
+  </>
+
+
+
 };
 
 export default Capacitacion;
+
+// Card https://react-bootstrap.netlify.app/docs/components/cards
+
+//Carousels https://react-bootstrap.netlify.app/docs/components/carousel sirve para mostrar imagenes en secuencia perfecto para el inicio de secion o registrpo dedatos 
+
+//Close button https://react-bootstrap.netlify.app/docs/components/close-button 
+
+//Images https://react-bootstrap.netlify.app/docs/components/images para imagenes
+
+// Modals https://react-bootstrap.netlify.app/docs/components/modal  sirve para dar ventanas emergentes 
+
+// Navbars https://react-bootstrap.netlify.app/docs/components/navbar barra que desglosa mas cosas
+
+//Offcanvas https://react-bootstrap.netlify.app/docs/components/offcanvas  Desglosa la barra lateral izquierda
+
+//PLaceholders https://react-bootstrap.netlify.app/docs/components/placeholder 
+
+// Tabbed components  https://react-bootstrap.netlify.app/docs/components/tabs 
+
+//Toast https://react-bootstrap.netlify.app/docs/components/toasts Sirve para mandar mensajes
